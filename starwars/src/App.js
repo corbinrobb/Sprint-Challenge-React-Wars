8d4f55cb2d1e16208234d05395e1907238338abc
Corbin Robb <corbinrobb@gmail.com>
2020-04-17 09:51:07 -0600

    Meet MVP and start search bar

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import axios from 'axios';
+import { Container } from 'reactstrap';
+import Character from './components/Character';
+import SearchBar from './components/SearchBar';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [ characters, setCharacters ] = useState([]);
+  const [ searchTerm, setSearchTerm ] = useState('');
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character')
+      .then(res => {
+        console.log(res.data.results)
+        setCharacters(res.data.results)
+      })
+      .catch(err => console.log(err));
+  }, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <Container className="card-container">
+        {characters.map(character => {
+          return <Character key={character.id} character={character} />
+        })}
+      </Container>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import React from 'react';
+import { Card, CardImg, CardTitle, CardText, CardSubtitle } from 'reactstrap';
+
+
+const Character = props => {
+  return (
+    <Card>
+      <CardImg src={props.character.image}></CardImg>
+      <CardTitle>{props.character.name}</CardTitle>
+      <CardSubtitle>{props.character.status}</CardSubtitle>
+      <CardSubtitle>{props.character.species}</CardSubtitle>
+      <CardSubtitle>{props.character.gender}</CardSubtitle>
+      <CardSubtitle>{props.character.origin.name}</CardSubtitle>
+    </Card>
+  );
+}
+
+
+export default Character;
